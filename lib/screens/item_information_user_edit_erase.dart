77f80fa93d6a164ec6b080cbb53003b42ae59e42
yeowonlee04 <yeowonlee04@gmail.com>
2025-05-06 21:51:42 +0900

    item_information_user_edit_erase.dart (본인 글 수정삭제 가능 화면) 완료

---
+++ lib/screens/item_information_user_edit_erase.dart
@@ -0,0 +1,226 @@
+import 'package:flutter/material.dart';
+import 'wishList.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'find_found3.dart';
+import 'find_found4.dart';
+
+
+class ItemInformationUserEditErase extends StatefulWidget {
+  @override
+  State<ItemInformationUserEditErase> createState() => _ItemInformationUserEditEraseState();
+}
+
+class _ItemInformationUserEditEraseState extends State<ItemInformationUserEditErase> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          IconButton(onPressed: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Wishlist()),
+            );
+          }, icon: Icon(Icons.favorite, color: Colors.white,)),
+          IconButton(onPressed: (){}, icon: Icon(Icons.notifications, color: Colors.white,)),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 285,
+            width: double.infinity,
+            color: Colors.grey[300],
+          ),
+
+          // 본문 내용
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  const SizedBox(height: 20),
+
+                  // 제목
+                  const Text(
+                    '저 진짜 너무 힘드네요.. 힘드네요..',
+                    style: TextStyle(
+                      color: Color(0xFF212121),
+                      fontSize: 19,
+                      fontFamily: 'Pretendard Variable',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+
+                  const SizedBox(height: 8),
+                  const Text(
+                    '분류/아찌고/똥    5분 전',
+                    style: TextStyle(
+                      color: Color(0xFFAEAEAE),
+                      fontSize: 13,
+                      fontFamily: 'Pretendard Variable',
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+
+                  const SizedBox(height: 14),
+                  const Divider(color: Color(0xFFDDDDDD), thickness: 1),
+
+                  // 프로필 정보
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 7),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          height: 54,
+                          width: 54,
+                          child: SvgPicture.asset(
+                            'assets/images/profileImage_noCamera.svg', // 너의 실제 SVG 경로로 바꿔줘
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+
+                        const SizedBox(width: 19),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              '닉네임',
+                              style: TextStyle(
+                                color: Color(0xFF212121),
+                                fontSize: 16,
+                                fontFamily: 'Pretendard Variable',
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              '위치',
+                              style: TextStyle(
+                                color:  Color(0xFFAEAEAE),
+                                fontSize: 13,
+                                fontFamily: 'Pretendard Variable',
+                                fontWeight: FontWeight.w400,
+                              ),
+                            )                          ],
+                        )
+                      ],
+                    ),
+                  ),
+
+                  const Divider(color: Color(0xFFDDDDDD), thickness: 1),
+
+                  const SizedBox(height: 20),
+
+                  // 본문
+                  const Text(
+                    '본문 내용\n\n가나다라마바사 아자차카타파하\n디자인 차력쇼를 보여드리겠습니다\n\n안녕하세요 감사합니다 영어로 땡큐 중국어로 쎼쎼\n\n반택만 합니다\n직거래 안합니다\n\n하자 오염 없습니다\n\n커어어억.... 드렁슨',
+                    style: TextStyle(
+                      color:  Color(0xFF212121),
+                      fontSize: 16,
+                      fontFamily: 'Pretendard Variable',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push( //찾아요라면 findfound3,찾았어요라면 findfound4로
+                            context,
+                            MaterialPageRoute(builder: (_) => FindFound3()),
+                          );
+                        },
+                        child: const Text(
+                          '수정',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      TextButton(
+                        onPressed: () {
+                          // 삭제 시 수행
+                        },
+                        child: const Text(
+                          '삭제',
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      // 하단 고정 영역: bottomNavigationBar로 이동
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Divider(
+            height: 1,
+            thickness: 1,
+            color: Color(0xFFDDDDDD), // 연회색 구분선
+          ),
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Row(
+              children: [
+                Row(
+                  children: const [
+                    Icon(Icons.favorite, color: Colors.grey),
+                    SizedBox(width: 26.23),
+                    Text(
+                      '현상금 얼마',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: () {
+                    // 채팅 페이지 이동 등
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFB5FFFF),
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: const Text(
+                    '채팅하기',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'Pretendard Variable',
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
